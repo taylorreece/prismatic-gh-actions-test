87c67508fd40efb9452674c4ecf7e4257da7c42a
Taylor Reece <taylor.reece@prismatic.io>
2024-07-24 16:20:48 -0500

    Bump to integration publisher version v1.4

--- integrations/acme-code-native/src/configPages.ts
+++ integrations/acme-code-native/src/configPages.ts
@@ -3,7 +3,7 @@ import { configPage, connectionConfigVar } from "@prismatic-io/spectral";
 export const configPages = {
   Connections: configPage({
     elements: {
-      "connection1": connectionConfigVar({
+      connection1: connectionConfigVar({
         stableKey: "cd91b40b-d6b3-4671-a563-c66198329625",
         dataType: "connection",
         inputs: {
